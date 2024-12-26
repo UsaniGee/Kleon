d9c86d74015c187a4fb6729e7eacdb43bb9a13b8
UsaniGee <usani3121@gmail.com>
2024-12-26 10:30:53 +0100

    Update

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,7 +6,7 @@ import Toggle from '../toggle'
 const Navbar = () => {
 
   return (
-    <div className=' flex justify-between px-5 lg:px-[100px] items-center py-2 shadow-md text-gray-400 dark:text-white font-semibold z-20 fixed w-full bg-white dark:bg-inherit pb-6'>
+    <div className=' flex justify-between px-5 lg:px-[100px] items-center py-2 shadow-md text-gray-400 dark:text-white font-semibold z-20 fixed w-full bg-white dark:bg-inherit pb-6 cursor-default'>
       <Logo />
       <div className='flex md:w-1/2 justify-between items-center gap-5'>
       <Links />
