1145cb662342f26787e05ee81246e17c8caa9de4
UsaniGee <usani3121@gmail.com>
2024-12-27 10:10:02 +0100

    corrections

--- src/components/navbar/logo/index.jsx
+++ src/components/navbar/logo/index.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Logo = () => {
   return (
     <Link to={'/'}>
-    <div className="flex items-center mb-4 md:mb-0 ">
-      <img className="aspect-square w-[20%] mr-2" src="assets/K.png" alt="Logo" />
+    <div className="flex items-center mb-4 md:mb-0 pt-2">
+      <img className="aspect-squar w-[25%] mr-2 object-cover" src="assets/K.png" alt="Logo" />
     </div></Link>
   )
 }
